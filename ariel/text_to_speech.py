edd7f123e9f9c374f4224b383557d0162cf2cfa6
Kacper Krasowiak <krasowiak@google.com>
2024-06-18 15:55:12 +0000

    add: Added the functions used to dub and process identified and translated utterances.
    Change-Id: I4c781d106cab9a54c517447818ade94fe1228ce5

--- ariel/text_to_speech.py
+++ ariel/text_to_speech.py
@@ -1,7 +1,9 @@
 """A text-to-speech module of of Ariel package from the Google EMEA gTech Ads Data Science."""
 
+import os
 from typing import Final, Mapping, Sequence
 from google.cloud import texttospeech
+from pydub import AudioSegment
 
 _SSML_MALE: Final[str] = "Male"
 _SSML_FEMALE: Final[str] = "Female"
@@ -15,6 +17,9 @@ _PREFERRED_VOICES: Final[Sequence[str]] = (
     "Neural2",
     "Standard",
 )
+DEFAULT_PITCH: Final[float] = -4.5
+DEFAULT_VOLUME_GAIN_DB: Final[float] = 16.0
+DEFAULT_SPEAKING_RATE: Final[float] = 1.0
 
 
 def list_available_voices(
@@ -56,8 +61,8 @@ def assign_voices(
   """Assigns voices to speakers based on preferred voices and available voices.
 
   Args:
-      utterance_metadata: A sequence of utterance metadata, each represented as a
-        dictionary with keys: 'start', 'end', 'chunk_path', 'translated_text',
+      utterance_metadata: A sequence of utterance metadata, each represented as
+        a dictionary with keys: 'start', 'end', 'chunk_path', 'translated_text',
         'speaker_id' and 'ssml_gender'.
       target_language: The target language (ISO 3166-1 alpha-2).
       client: A TextToSpeechClient object.
@@ -116,3 +121,143 @@ def assign_voices(
   return voice_assignment
 
 
+def update_utterance_metadata(
+    *,
+    utterance_metadata: Sequence[Mapping[str, str | float]],
+    assigned_voices: Mapping[str, str],
+) -> Sequence[Mapping[str, str | float]]:
+  """Updates utterance metadata with assigned Google voices.
+
+  Args:
+      utterance_metadata: A sequence of utterance metadata, each represented as
+        a dictionary with keys: 'start', 'end', 'chunk_path', 'translated_text',
+        'speaker_id' and 'ssml_gender'.
+      assigned_voices: Mapping mapping speaker IDs to assigned Google voices.
+
+  Returns:
+      Sequence of updated utterance metadata dictionaries.
+  """
+  updated_chunks = []
+  for metadata_item in utterance_metadata:
+    new_utterance = metadata_item.copy()
+    speaker_id = new_utterance.get("speaker_id")
+    new_utterance["assigned_google_voice"] = assigned_voices.get(speaker_id)
+    updated_chunks.append(new_utterance)
+  return updated_chunks
+
+
+def convert_text_to_speech(
+    client: texttospeech.TextToSpeechClient,
+    assigned_google_voice: str,
+    target_language: str,
+    output_filename: str,
+    text: str,
+    pitch: float = DEFAULT_PITCH,
+    volume_gain_db: float = DEFAULT_VOLUME_GAIN_DB,
+    speaking_rate: float = DEFAULT_SPEAKING_RATE,
+) -> str:
+  """Converts text to speech using Google Cloud Text-to-Speech API.
+
+  Args:
+      client: The GCP text to speech client object to use.
+      assigned_google_voice: The name of the Google Cloud voice to use.
+      target_language: The target language (ISO 3166-1 alpha-2).
+      output_filename: The name of the output MP3 file.
+      text: The text to be converted to speech.
+      pitch: The pitch of the synthesized speech.
+      volume_gain_db: The volume gain of the synthesized speech.
+      speaking_rate: The speaking rate of the synthesized speech.
+
+  Returns:
+      The name of the output file.
+  """
+
+  input_text = texttospeech.SynthesisInput(text=text)
+  voice_selection = texttospeech.VoiceSelectionParams(
+      name=assigned_google_voice,
+      language_code=target_language,
+  )
+  audio_config = texttospeech.AudioConfig(
+      audio_encoding=texttospeech.AudioEncoding.MP3,
+      pitch=pitch,
+      volume_gain_db=volume_gain_db,
+      speaking_rate=speaking_rate,
+  )
+
+  response = client.synthesize_speech(
+      input=input_text,
+      voice=voice_selection,
+      audio_config=audio_config,
+  )
+
+  with open(output_filename, "wb") as out:
+    out.write(response.audio_content)
+
+  return output_filename
+
+
+def adjust_audio_speed(*, input_mp3_path: str, target_duration: float) -> None:
+  """Adjusts the speed of an MP3 file to match the target duration.
+
+  Args:
+      input_mp3_path: The path to the input MP3 file.
+      target_duration: The desired duration in seconds.
+  """
+  if target_duration <= 0.0:
+    raise ValueError(
+        "The target duration must be more than 0.0 seconds. Got"
+        f" {target_duration}."
+    )
+  audio = AudioSegment.from_mp3(input_mp3_path)
+  if audio.duration_seconds <= 0.0:
+    raise ValueError(
+        "The input audio duration must be more than 0.0 seconds. It's"
+        f" {audio.duration_seconds}."
+    )
+  speed_factor = audio.duration_seconds / target_duration
+  new_audio = audio.speedup(speed_factor)
+  new_audio.export(input_mp3_path, format="mp3")
+
+
+def dub_utterances(
+    *,
+    client: texttospeech.TextToSpeechClient,
+    utterance_metadata: Sequence[Mapping[str, str | float]],
+    output_directory: str,
+    target_language: str,
+) -> Sequence[Mapping[str, str | float]]:
+  """Processes a list of chunks, generating dubbed audio files.
+
+  Args:
+      client: The TextToSpeechClient object to use.
+      utterance_metadata: A sequence of utterance metadata, each represented as
+        a dictionary with keys: "start", "end", "chunk_path", "translated_text",
+        "speaker_id" and "ssml_gender".
+      output_directory: Path to the directory for output files.
+      target_language: The target language (ISO 3166-1 alpha-2).
+
+  Returns:
+      List of processed chunks with updated "dubbed_path".
+  """
+
+  processed_chunks = []
+  for chunk in utterance_metadata:
+    chunk_path = chunk["chunk_path"]
+    text = chunk["translated_text"]
+    assigned_google_voice = chunk["speaker_id"]
+    duration = chunk["end"] - chunk["start"]
+    base_filename = os.path.splitext(os.path.basename(chunk_path))[0]
+    output_filename = os.path.join(
+        output_directory, f"dubbed_{base_filename}.mp3"
+    )
+    dubbed_path = convert_text_to_speech(
+        client=client,
+        assigned_google_voice=assigned_google_voice,
+        target_language=target_language,
+        output_filename=output_filename,
+        text=text,
+    )
+    adjust_audio_speed(input_mp3_path=dubbed_path, target_duration=duration)
+    chunk["dubbed_path"] = dubbed_path
+    processed_chunks.append(chunk)
+  return processed_chunks
--- tests/text_to_speech_test.py
+++ tests/text_to_speech_test.py
@@ -1,13 +1,15 @@
 """Tests for utility functions in text_to_speech.py."""
 
 from collections import namedtuple
+import tempfile
+from tempfile import NamedTemporaryFile
+from unittest import mock
 from unittest.mock import MagicMock, patch
 from absl.testing import absltest
 from absl.testing import parameterized
 from ariel import text_to_speech
 from google.cloud import texttospeech
-
-AvailableVoice = namedtuple("AvailableVoice", ["name", "ssml_gender"])
+from pydub import AudioSegment
 
 
 class ListAvailableVoicesTest(absltest.TestCase):
@@ -106,6 +108,174 @@ class TestAssignVoices(parameterized.TestCase):
         fallback_no_preferred_category_match=fallback_no_preferred_category_match,
     )
     self.assertEqual(assignment, expected_assignment)
+
+
+class UpdateUtteranceMetadataTest(absltest.TestCase):
+
+  def test_update_utterance_metadata(self):
+    utterance_metadata = [
+        {
+            "start": 0.0,
+            "end": 1.0,
+            "chunk_path": "path/to/chunk1.wav",
+            "translated_text": "Hello",
+            "speaker_id": "speaker1",
+            "ssml_gender": "male",
+        },
+        {
+            "start": 1.0,
+            "end": 2.0,
+            "chunk_path": "path/to/chunk2.wav",
+            "translated_text": "World",
+            "speaker_id": "speaker2",
+            "ssml_gender": "female",
+        },
+    ]
+    assigned_voices = {
+        "speaker1": "en-US-Wavenet-C",
+        "speaker2": "en-US-Wavenet-F",
+    }
+    expected_output = [
+        {
+            "start": 0.0,
+            "end": 1.0,
+            "chunk_path": "path/to/chunk1.wav",
+            "translated_text": "Hello",
+            "speaker_id": "speaker1",
+            "ssml_gender": "male",
+            "assigned_google_voice": "en-US-Wavenet-C",
+        },
+        {
+            "start": 1.0,
+            "end": 2.0,
+            "chunk_path": "path/to/chunk2.wav",
+            "translated_text": "World",
+            "speaker_id": "speaker2",
+            "ssml_gender": "female",
+            "assigned_google_voice": "en-US-Wavenet-F",
+        },
+    ]
+    actual_output = text_to_speech.update_utterance_metadata(
+        utterance_metadata=utterance_metadata, assigned_voices=assigned_voices
+    )
+    self.assertEqual(actual_output, expected_output)
+
+
+class TestConvertTextToSpeech(absltest.TestCase):
+
+  def test_convert_text_to_speech(self):
+    mock_client = MagicMock(spec=texttospeech.TextToSpeechClient)
+    mock_response = texttospeech.SynthesizeSpeechResponse(
+        audio_content=b"mock_audio_data"
+    )
+    mock_client.synthesize_speech.return_value = mock_response
+    with tempfile.NamedTemporaryFile(suffix=".mp3") as temporary_file:
+      output_file = temporary_file.name
+      result = text_to_speech.convert_text_to_speech(
+          mock_client,
+          "en-US-Wavenet-A",
+          "en-US",
+          output_file,
+          "This is a test.",
+      )
+      self.assertEqual(result, output_file)
+      mock_client.synthesize_speech.assert_called_once()
+
+
+class TestAdjustAudioSpeed(absltest.TestCase):
+
+  def test_adjust_audio_speed_positive_duration(self):
+    with tempfile.NamedTemporaryFile(suffix=".mp3") as temp_file:
+      sample_audio = AudioSegment.silent(duration=5.0 * 1000)
+      sample_audio.export(temp_file.name, format="mp3")
+      target_duration = 3.0
+      text_to_speech.adjust_audio_speed(
+          input_mp3_path=temp_file.name, target_duration=target_duration
+      )
+      adjusted_audio = AudioSegment.from_mp3(temp_file.name)
+      self.assertAlmostEqual(
+          adjusted_audio.duration_seconds, target_duration, delta=0.2
+      )
+
+  def test_adjust_audio_speed_zero_duration(self):
+    with tempfile.NamedTemporaryFile(suffix=".mp3") as temp_file:
+      sample_audio = AudioSegment.silent(duration=5.0 * 1000)
+      sample_audio.export(temp_file.name, format="mp3")
+      target_duration = 0.0
+      with self.assertRaisesRegex(
+          ValueError,
+          "The target duration must be more than 0.0 seconds. Got"
+          f" {target_duration}.",
+      ):
+        text_to_speech.adjust_audio_speed(
+            input_mp3_path=temp_file.name, target_duration=target_duration
+        )
+
+
+class TestDubUtterances(absltest.TestCase):
+
+  def setUp(self):
+    self.mock_adjust_audio_speed = MagicMock()
+    self.mock_client = MagicMock(spec=texttospeech.TextToSpeechClient)
+    self.original_convert_text_to_speech = text_to_speech.convert_text_to_speech
+    self.original_adjust_audio_speed = text_to_speech.adjust_audio_speed
+    text_to_speech.adjust_audio_speed = self.mock_adjust_audio_speed
+
+  def tearDown(self):
+    text_to_speech.convert_text_to_speech = self.original_convert_text_to_speech
+    text_to_speech.adjust_audio_speed = self.original_adjust_audio_speed
+
+  def test_dub_utterances(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      utterance_metadata = [
+          {
+              "start": 0.0,
+              "end": 5.5,
+              "chunk_path": "chunk_1.wav",
+              "translated_text": "This is dubbed text 1.",
+              "speaker_id": "en-US-Wavenet-A",
+          },
+          {
+              "start": 5.5,
+              "end": 12.0,
+              "chunk_path": "chunk_2.wav",
+              "translated_text": "This is dubbed text 2.",
+              "speaker_id": "en-US-Wavenet-B",
+          },
+      ]
+
+      def mock_convert_text_to_speech(
+          client, assigned_google_voice, target_language, output_filename, text
+      ):
+        del client, assigned_google_voice, target_language, text
+        return output_filename
+
+      text_to_speech.convert_text_to_speech = mock_convert_text_to_speech
+      result = text_to_speech.dub_utterances(
+          client=self.mock_client,
+          utterance_metadata=utterance_metadata,
+          output_directory=temp_dir,
+          target_language="en-US",
+      )
+      expected_output = [
+          {
+              "start": 0.0,
+              "end": 5.5,
+              "chunk_path": "chunk_1.wav",
+              "translated_text": "This is dubbed text 1.",
+              "speaker_id": "en-US-Wavenet-A",
+              "dubbed_path": f"{temp_dir}/dubbed_chunk_1.mp3",
+          },
+          {
+              "start": 5.5,
+              "end": 12.0,
+              "chunk_path": "chunk_2.wav",
+              "translated_text": "This is dubbed text 2.",
+              "speaker_id": "en-US-Wavenet-B",
+              "dubbed_path": f"{temp_dir}/dubbed_chunk_2.mp3",
+          },
+      ]
+      self.assertEqual(result, expected_output)
 
 
 if __name__ == "__main__":
