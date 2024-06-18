c15fd537c26e5bdc15ff53417620cc2a6be0a801
Kacper Krasowiak <krasowiak@google.com>
2024-06-18 11:53:40 +0000

    add: Added the functions to add translations to the utterance metadata and merge utterances when necessary.
    Change-Id: I11f0749eae363cfb4263d8bfa15eecc05dbba845

--- ariel/translation.py
+++ ariel/translation.py
@@ -1,5 +1,6 @@
 """A translation module of Ariel package from the Google EMEA gTech Ads Data Science."""
 
+import re
 from typing import Final, Mapping, Sequence
 import google.generativeai as genai
 
@@ -7,6 +8,7 @@ _TRANSLATION_PROMPT: Final[str] = (
     "You're hired by a company called: {}. The received transcript is: {}."
     " Specific instructions: {}. The target language is: {}."
 )
+_TIMESTAMP_THRESHOLD: Final[float] = 0.001
 
 
 def generate_script(
@@ -56,3 +58,87 @@ def translate_script(
   response = translation_chat_session.send_message(prompt)
   translation_chat_session.rewind()
   return response.text
+
+
+def add_translations(
+    utterance_metadata: Sequence[Mapping[str, str | float]], text_string: str
+) -> Sequence[Mapping[str, str | float]]:
+  """Adds the "translated_text" field of each utterance.
+
+  Args:
+      utterance_metadata: The sequence of mappings, where each mapping
+        represents utterance metadata with "text", "start", "stop",
+        "speaker_id", "ssml_gender" keys.
+      text_string: The string containing the translated text segments, separated
+        by "<BREAK>".
+
+  Returns:
+      A list of updated utterance metadata with the "translated_text" field
+      populated.
+
+  Raises:
+      ValueError: If the number of utterance metadata and text segments do not
+      match.
+  """
+  text_string = re.sub(r"\s*<BREAK>\s*", "<BREAK>", text_string).rstrip()
+  text_segments = [
+      segment for segment in text_string.split("<BREAK>") if segment
+  ]
+  if len(utterance_metadata) != len(text_segments):
+    raise ValueError(
+        "The utterance metadata must be of the same length as the text"
+        f" segments. Currently they are: {len(utterance_metadata)} and"
+        f" {len(text_segments)}."
+    )
+  updated_utterance_metadata = []
+  for metadata, translated_text in zip(utterance_metadata, text_segments):
+    if translated_text != "<DO NOT TRANSLATE>":
+      updated_utterance_metadata.append(
+          {**metadata, "translated_text": translated_text}
+      )
+    else:
+      continue
+  return updated_utterance_metadata
+
+
+def merge_utterances(
+    utterance_metadata: Sequence[Mapping[str, str | float]],
+    timestamp_threshold: float = _TIMESTAMP_THRESHOLD,
+) -> Sequence[Mapping[str, str | float]]:
+  """Merges utterances that are within the specified timestamp threshold.
+
+  The function looks at
+
+  Args:
+    utterance_metadata: A sequence of utterance metadata, each represented as a
+      dictionary with keys: 'start', 'end', 'chunk_path', and 'translated_text'.
+    timestamp_threshold: The maximum time difference between the end of one
+      utterance and the start of the next for them to be considered mergeable.
+
+  Returns:
+    A list of merged utterance metadata.
+  """
+
+  merged_utterances = []
+  index = 0
+  while index < len(utterance_metadata):
+    current_utterance = utterance_metadata[index]
+    merged_utterance = current_utterance.copy()
+    next_index = index + 1
+    while (
+        next_index < len(utterance_metadata)
+        and utterance_metadata[next_index]["start"] - current_utterance["end"]
+        < timestamp_threshold
+    ):
+      merged_utterance["chunk_path"] = tuple(
+          [merged_utterance["chunk_path"]]
+          + [utterance_metadata[next_index]["chunk_path"]]
+      )
+      merged_utterance["end"] = utterance_metadata[next_index]["end"]
+      merged_utterance[
+          "translated_text"
+      ] += f" {utterance_metadata[next_index]['translated_text']}"
+      next_index += 1
+    merged_utterances.append(merged_utterance)
+    index = next_index
+  return merged_utterances
--- tests/translation_test.py
+++ tests/translation_test.py
@@ -10,37 +10,37 @@ import google.generativeai as genai
 class GenerateScriptTest(parameterized.TestCase):
 
   @parameterized.named_parameters(
-      ('empty_input', [], ''),
+      ("empty_input", [], ""),
       (
-          'single_utterance',
+          "single_utterance",
           [{
-              'text': 'Hello, world!',
-              'start': 0.0,
-              'stop': 1.0,
-              'speaker_id': 'speaker1',
-              'ssml_gender': 'male',
+              "text": "Hello, world!",
+              "start": 0.0,
+              "stop": 1.0,
+              "speaker_id": "speaker1",
+              "ssml_gender": "male",
           }],
-          'Hello, world!',
+          "Hello, world!",
       ),
       (
-          'multiple_utterances',
+          "multiple_utterances",
           [
               {
-                  'text': 'This is',
-                  'start': 0.0,
-                  'stop': 1.0,
-                  'speaker_id': 'speaker1',
-                  'ssml_gender': 'male',
+                  "text": "This is",
+                  "start": 0.0,
+                  "stop": 1.0,
+                  "speaker_id": "speaker1",
+                  "ssml_gender": "male",
               },
               {
-                  'text': 'a test.',
-                  'start': 1.0,
-                  'stop': 2.0,
-                  'speaker_id': 'speaker2',
-                  'ssml_gender': 'female',
+                  "text": "a test.",
+                  "start": 1.0,
+                  "stop": 2.0,
+                  "speaker_id": "speaker2",
+                  "ssml_gender": "female",
               },
           ],
-          'This is <BREAK> a test.',
+          "This is <BREAK> a test.",
       ),
   )
   def test_generate_script(self, utterance_metadata, expected_script):
@@ -55,17 +55,207 @@ class TranslateScriptTest(absltest.TestCase):
   def test_translate_script(self):
     mock_model = MagicMock(spec=genai.GenerativeModel)
     mock_chat_session = MagicMock()
-    mock_chat_session.send_message.return_value = MagicMock(text='Test.')
+    mock_chat_session.send_message.return_value = MagicMock(text="Test.")
     mock_model.start_chat.return_value = mock_chat_session
     translation_output = translation.translate_script(
-        transcript='Test.',
-        company_name='Advertiser Name',
-        translation_instructions='Translate to Polish.',
-        target_language='pl-PL',
+        transcript="Test.",
+        company_name="Advertiser Name",
+        translation_instructions="Translate to Polish.",
+        target_language="pl-PL",
         model=mock_model,
     )
-    self.assertEqual(translation_output, 'Test.')
+    self.assertEqual(translation_output, "Test.")
 
 
-if __name__ == '__main__':
+class AddTranslationsTest(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      (
+          "valid_input",
+          [
+              {
+                  "text": "Hello",
+                  "start": 0.0,
+                  "stop": 1.0,
+                  "speaker_id": "speaker1",
+                  "ssml_gender": "male",
+              },
+              {
+                  "text": "World",
+                  "start": 1.0,
+                  "stop": 2.0,
+                  "speaker_id": "speaker2",
+                  "ssml_gender": "female",
+              },
+          ],
+          "Bonjour<BREAK>Le Monde",
+          [
+              {
+                  "text": "Hello",
+                  "start": 0.0,
+                  "stop": 1.0,
+                  "speaker_id": "speaker1",
+                  "ssml_gender": "male",
+                  "translated_text": "Bonjour",
+              },
+              {
+                  "text": "World",
+                  "start": 1.0,
+                  "stop": 2.0,
+                  "speaker_id": "speaker2",
+                  "ssml_gender": "female",
+                  "translated_text": "Le Monde",
+              },
+          ],
+      ),
+      (
+          "do_not_translate",
+          [
+              {
+                  "text": "Hello",
+                  "start": 0.0,
+                  "stop": 1.0,
+                  "speaker_id": "speaker1",
+                  "ssml_gender": "male",
+              },
+              {
+                  "text": "World",
+                  "start": 1.0,
+                  "stop": 2.0,
+                  "speaker_id": "speaker2",
+                  "ssml_gender": "female",
+              },
+          ],
+          "<DO NOT TRANSLATE><BREAK>Le Monde",
+          [
+              {
+                  "text": "World",
+                  "start": 1.0,
+                  "stop": 2.0,
+                  "speaker_id": "speaker2",
+                  "ssml_gender": "female",
+                  "translated_text": "Le Monde",
+              },
+          ],
+      ),
+  )
+  def test_add_translations(
+      self, utterance_metadata, text_string, expected_translated_metadata
+  ):
+    updated_metadata = translation.add_translations(
+        utterance_metadata, text_string
+    )
+    self.assertEqual(updated_metadata, expected_translated_metadata)
+
+  def test_add_translations_with_mismatched_length(self):
+    utterance_metadata = [
+        {
+            "text": "Hello",
+            "start": 0.0,
+            "stop": 1.0,
+            "speaker_id": "speaker1",
+            "ssml_gender": "male",
+        },
+    ]
+    text_string = "Bonjour<BREAK>Le Monde<BREAK>Another Segment"
+    with self.assertRaisesRegex(
+        ValueError, "The utterance metadata must be of the same length"
+    ):
+      translation.add_translations(utterance_metadata, text_string)
+
+
+class MergeUtterancesTest(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      (
+          "merge_within_threshold",
+          [
+              {
+                  "start": 0.0,
+                  "end": 1.0,
+                  "chunk_path": "a.wav",
+                  "translated_text": "Hello",
+              },
+              {
+                  "start": 1.1,
+                  "end": 2.0,
+                  "chunk_path": "b.wav",
+                  "translated_text": "world",
+              },
+              {
+                  "start": 2.1,
+                  "end": 3.0,
+                  "chunk_path": "c.wav",
+                  "translated_text": "!",
+              },
+          ],
+          0.2,
+          [
+              {
+                  "start": 0.0,
+                  "end": 2.0,
+                  "translated_text": "Hello world",
+                  "chunk_path": ("a.wav", "b.wav"),
+              },
+              {
+                  "start": 2.1,
+                  "end": 3.0,
+                  "translated_text": "!",
+                  "chunk_path": "c.wav",
+              },
+          ],
+      ),
+      (
+          "no_merge_above_threshold",
+          [
+              {
+                  "start": 0.0,
+                  "end": 1.0,
+                  "chunk_path": "a.wav",
+                  "translated_text": "Hello",
+              },
+              {
+                  "start": 1.5,
+                  "end": 2.0,
+                  "chunk_path": "b.wav",
+                  "translated_text": "world",
+              },
+              {
+                  "start": 2.1,
+                  "end": 3.0,
+                  "chunk_path": "c.wav",
+                  "translated_text": "!",
+              },
+          ],
+          0.1,
+          [
+              {
+                  "start": 0.0,
+                  "end": 1.0,
+                  "chunk_path": "a.wav",
+                  "translated_text": "Hello",
+              },
+              {
+                  "start": 1.5,
+                  "end": 2.0,
+                  "chunk_path": "b.wav",
+                  "translated_text": "world",
+              },
+              {
+                  "start": 2.1,
+                  "end": 3.0,
+                  "chunk_path": "c.wav",
+                  "translated_text": "!",
+              },
+          ],
+      ),
+  )
+  def test_merge_utterances(
+      self, utterances, timestamp_threshold, expected_merged_utterances
+  ):
+    merged = translation.merge_utterances(utterances, timestamp_threshold)
+    self.assertSequenceEqual(merged, expected_merged_utterances)
+
+
+if __name__ == "__main__":
   absltest.main()
