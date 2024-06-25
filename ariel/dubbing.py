a5d84d011ea2cff44ba08c18409442f03dbcab58
Kacper Krasowiak <krasowiak@google.com>
2024-06-25 09:15:05 +0000

    fix: Changed the license type following the offline thread with the open source team.
    Change-Id: Id6d00724c43f10be201de22dcac54d1abec885e0

--- ariel/dubbing.py
+++ ariel/dubbing.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google LLC.
+#
+# Licensed under the MIT License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.mit.edu/~amini/LICENSE.md
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """A dubbing module of Ariel package from the Google EMEA gTech Ads Data Science."""
 
 import dataclasses
@@ -112,7 +126,11 @@ def read_system_settings(system_instructions: str) -> str:
       ) as assets_directory:
         file_path = os.path.join(assets_directory, system_instructions)
         with tf.io.gfile.GFile(file_path, "r") as file:
-          return file.read()
+          result = []
+          for line in file:
+              if not line.lstrip().startswith("#"):
+                  result.append(line)
+          return "".join(result)
     except Exception:
       raise ValueError(
           "You specified a .txt file that's not part of the Ariel package."
