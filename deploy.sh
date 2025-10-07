392484edaf31a794a4992c2c5bb067028009de6d
Michael Weiler <michaelweiler@google.com>
2025-10-07 15:47:31 +0200

    update deploy script to stream logs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+SERVICE_NAME="ariel"
+REGION="us-central1"
+
 # Build requirements.txt needed for cloud run but skipping the local file output from uv
-pipx run uv pip compile pyproject.toml -o requirements.txt
+pipx run uv pip compile pyproject.toml -o requirements.txt > /dev/null 2>&1
 
 # Deploy and capture the exit code
-gcloud beta run deploy ariel --source . --region=us-central1 --quiet --iap
+gcloud beta run deploy "$SERVICE_NAME" --source . --region="$REGION" --quiet --iap
+
+# Stream logs
+gcloud beta run services logs tail "$SERVICE_NAME" --region="$REGION"
