f5ad1e43616ccc377c53b1992c7290200b9b2d11
Michael Weiler <michaelweiler@google.com>
2025-10-07 13:56:10 +0200

    adding deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-uv pip freeze > requirements.txt
-gcloud run deploy ariel --source . --region=us-central1 --quiet
+# Build requirements.txt needed for cloud run but skipping the local file output from uv
+pipx run uv pip compile pyproject.toml -o requirements.txt
+
+# Deploy and capture the exit code
+gcloud beta run deploy ariel --source . --region=us-central1 --quiet --iap
