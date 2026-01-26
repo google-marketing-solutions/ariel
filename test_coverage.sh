6eed662543e5fd0913387f1cb367d61267f18a73
Kuba Rożek <kubarozek@google.com>
2026-01-26 10:40:46 +0000

    feature: add test coverage script for Python code

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Copyright 2025 Google LLC
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy
 # of the License at
-
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,4 +15,8 @@
 # under the License.
 
 pip install -q -r requirements-dev.txt
-python -m unittest discover -v -s tests
+coverage run \
+  --source=. \
+  --omit="tests/*,.venv/*,*/site-packages/*" \
+  -m unittest discover -s tests \
+  && coverage report -m
