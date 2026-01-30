73c3ba779029f53ed5e25e025c0e24b5c7ae55be
Kuba Rożek <kubarozek@google.com>
2026-01-30 11:20:53 +0000

    fix: merge utils tests from mail

--- static/js/utils.test.js
+++ static/js/utils.test.js
@@ -57,5 +57,60 @@ describe('Utils', () => {
       const messages = checkOverlap(u1, [u1, u2]);
       expect(messages.length).toBe(0);
     });
+
+    it('NO overlap, should return empty array', () => {
+      // Arrange
+      const utterance = { id: 1, translated_start_time: 0, translated_end_time: 5 };
+      const otherUtterance = { id: 2, translated_start_time: 10, translated_end_time: 15 };
+      const allUtterances = [otherUtterance];
+      // Act
+      const result = checkOverlap(utterance, allUtterances);
+      // Assert
+      expect(result).toEqual([]);
+    });
+
+    it('PARTIAL OVERLAP, should return error message', () => {
+      // Arrange
+      const utterance = { id: 1, translated_start_time: 0, translated_end_time: 5 };
+      const otherUtterance = { id: 2, translated_start_time: 3, translated_end_time: 8 };
+      const allUtterances = [otherUtterance];
+      // Act
+      const result = checkOverlap(utterance, allUtterances);
+      // Assert
+      expect(result).toEqual(['Translated time overlaps with another utterance.']);
+    });
+
+    it('ENCLOSED OVERLAP, should return error message', () => {
+      // Arrange
+      const utterance = { id: 1, translated_start_time: 2, translated_end_time: 4 };
+      const otherUtterance = { id: 2, translated_start_time: 0, translated_end_time: 8 };
+      const allUtterances = [otherUtterance];
+      // Act
+      const result = checkOverlap(utterance, allUtterances);
+      // Assert
+      expect(result).toEqual(['Translated time overlaps with another utterance.']);
+    });
+
+    it('SAME ID, should return empty array', () => {
+      // Arrange
+      const utterance = { id: 1, translated_start_time: 2, translated_end_time: 4 };
+      const otherUtterance = { id: 1, translated_start_time: 2, translated_end_time: 4 };
+      const allUtterances = [otherUtterance];
+      // Act
+      const result = checkOverlap(utterance, allUtterances);
+      // Assert
+      expect(result).toEqual([]);
+    });
+
+    it('REMOVED UTTERANCE, should return empty array', () => {
+      // Arrange
+      const utterance = { id: 1, translated_start_time: 2, translated_end_time: 4, removed: true };
+      const otherUtterance = { id: 2, translated_start_time: 2, translated_end_time: 4 };
+      const allUtterances = [otherUtterance];
+      // Act
+      const result = checkOverlap(utterance, allUtterances);
+      // Assert
+      expect(result).toEqual([]);
+    });
   });
 });
