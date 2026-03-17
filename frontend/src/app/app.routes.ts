8775e30ce7bf17b9ea9c5c157cd5719a03e5c03e
Igor Juric <ijuric@google.com>
2026-03-17 10:02:56 +0100

    removing v2 from the name

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { Editor } from './editor/editor';
 import { Result } from './result/result';
 
 export const routes: Routes = [
-  { path: '', component: Home, title: 'Ariel v2.0 - Home' },
-  { path: 'library', component: Library, title: 'Ariel v2.0 - Library' },
-  { path: 'editor', component: Editor, title: 'Ariel v2.0 - Editor' },
-  { path: 'result', component: Result, title: 'Ariel v2.0 - Result' }
+  { path: '', component: Home, title: 'Ariel - Home' },
+  { path: 'library', component: Library, title: 'Ariel - Library' },
+  { path: 'editor', component: Editor, title: 'Ariel - Editor' },
+  { path: 'result', component: Result, title: 'Ariel - Result' }
 ];
